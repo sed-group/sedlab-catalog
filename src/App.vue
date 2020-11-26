cc450dd1037d6ea92d9a669239ef971bc88adc3e
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2020-11-26 22:28:59 +0100

    sedlab link

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,17 @@
     >
       <v-toolbar-title class="headline text-uppercase">
         <span>SED</span>
-        <span class="font-weight-light">Lab</span>
+        <span class="font-weight-light">Lab Catalog</span>
       </v-toolbar-title>
 
       <v-spacer></v-spacer>
 
       <v-btn
-        href="https://github.com/sed-group/sedlab-catalog"
+        href="https://sedlab-catalog.netlify.app/"
         target="_blank"
         text
       >
-        <span class="mr-2">GitHub</span>
+        <span class="mr-2">SEDLab</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
       <v-btn
